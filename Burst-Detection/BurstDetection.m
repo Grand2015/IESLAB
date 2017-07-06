73aee8c19ea4631ebbb8dabe60d4d3151c64a151
Grand2015 <997812940@qq.com>
2017-07-06 22:32:09 +0800

    add ksdensity function

--- Burst-Detection/BurstDetection.m
+++ Burst-Detection/BurstDetection.m
@@ -20,31 +20,53 @@
 clc;
 clear all;
 %% 载入4月3日数据数据
-load('C:\Users\hongwei\Desktop\IESLAB\SCADA-Data\observe4_3.mat');    
+% load('C:\Users\hongwei\Desktop\IESLAB\SCADA-Data\observe4_3.mat');   
+load('C:\Users\hongwei_lab\Desktop\IESLAB\SCADA-Data\observe4_3.mat')
 %% 原始数据插值处理
 lev = 3;
-for i=1:14
-observe4_3Smooth(:,i)= smooth(observe4_3(:,i));
-%% 小波降噪
-pre4_3Wden(:,i) = wden(observe4_3Smooth(:,i),'heursure','s','mln',lev,'sym8');
-%计算一列的平均值和标准差
-observe4_3average(1,i)=mean(pre4_3Wden(:,i));
-pre4_3(i,:)=pre4_3Wden(:,i)';
-observe4_3std(1,i)=std(pre4_3(i,:),1);
+monitorNum=14;%监测点个数
+sampleNum=1440;%采样点数，即1个/分钟
+for i = 1:monitorNum
+    %作差值处理
+    for j = 1:sampleNum-1
+        observe4_3diff(j,i) = observe4_3(j+1,i) - observe4_3(j,i);
+    end
+    observe4_3Smooth(:,i) = smooth(observe4_3diff(:,i));
+    % 小波降噪
+    observe4_3Wden(:,i) = wden(observe4_3Smooth(:,i),'heursure','s','mln',lev,'sym8');
+    %计算14个测点的平均值和标准差
+    observe4_3average(1,i)=mean(observe4_3Wden(:,i));
+    pre4_3T(i,:)=observe4_3Wden(:,i)';
+    observe4_3std(1,i)=std(pre4_3T(i,:),1);
 end
-%  x=-5:0.01:5;
-% hist(pre4_3(:,1),20); hold on;
-% a=randn(10000,1); 
-[b, x]=hist(pre4_3Wden(:,1),100);
-% % b = hist(pre4_3Wden(:,1),1001);
-num=numel(pre4_3Wden(:,1));
-figure;plot(x,b/num);   %概率密度
+
+hist(observe4_3Wden(:,1),100); hold on;
+[b, x]=hist(observe4_3Wden(:,1),100);
+num=numel(observe4_3Wden(:,1));
+
+miu=observe4_3average(1,1);
+sigma=observe4_3std(1,1);
+y1=normpdf(x,miu,sigma);
+y2=normcdf(x,miu,sigma);
+
+% [f,xi] = ksdensity(observe4_3Wden(:,1));
+% plot(xi,f);
+
+
+figure(1);
+subplot 211;
+plot(x,b/num);   %概率密度
+title('4月3日第一个测点压力变化正态分布的概率密度')
 c=cumsum(b/num);        %累积分布
-figure; plot(x,c);
-% miu=observe4_3average;
-% sigma=observe4_3std;
-% y1=normpdf(x,miu,sigma);
-% y2=normcdf(x,miu,sigma);
+% figure; 
+subplot 212;
+plot(x,c,'+',x,y2,'--');
+title('4月3日第一个测点压力变化正态分布的累积分布');
+figure(2);
+plot(x,c,'+',x,y2,'--');
+title('4月3日第一个测点压力变化正态分布的累积分布');
+xlabel('');ylabel('概率');
+
 % figure(1)  
 % subplot 211
 % plot(x,y1)
