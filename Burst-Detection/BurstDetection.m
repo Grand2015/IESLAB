46b41dcb4ca6a2117da62e4a414320fba5a3ee09
Li <hongwei.1.li.ext@nokia.com>
2017-07-11 13:06:52 +0800

    对14个测点压力差作正态分布分析

--- Burst-Detection/BurstDetection.m
+++ Burst-Detection/BurstDetection.m
@@ -1,31 +1,15 @@
-%% 正态分布累计概率
-% clear
-% x=-4:0.01:4;
-% miu=0;sigma=1;
-% y1=normpdf(x,miu,sigma);
-% y2=normcdf(x,miu,sigma);
-% figure(1) 
-% subplot 221
-% plot(x,y1)
-% title('正态分布的概率密度')
-% subplot 222
-% plot(x,y2)
-% title('正态分布的累积分布')
-
- %% point1 数据曲线
-% load('observe4_3.mat');
-% t=1:1:1440;
-% plot(t,observe4_3(:,1));
-
 clc;
 clear all;
+
 %% 载入4月3日数据数据
 % load('C:\Users\hongwei\Desktop\IESLAB\SCADA-Data\observe4_3.mat');   
 load('C:\Users\hongwei_lab\Desktop\IESLAB\SCADA-Data\observe4_3.mat')
+
 %% 原始数据插值处理
-lev = 3;
-monitorNum=14;%监测点个数
-sampleNum=1440;%采样点数，即1个/分钟
+lev  = 3;
+flag = 1;
+monitorNum = 14;    %监测点个数
+sampleNum  = 1440;  %采样点数，即1个/分钟
 for i = 1:monitorNum
     %作差值处理
     for j = 1:sampleNum-1
@@ -34,31 +18,52 @@ for i = 1:monitorNum
     observe4_3Smooth(:,i) = smooth(observe4_3diff(:,i));
     % 小波降噪
     observe4_3Wden(:,i) = wden(observe4_3Smooth(:,i),'heursure','s','mln',lev,'sym8');
-    %计算14个测点的平均值和标准差
-%     observe4_3average(1,i)=mean(observe4_3Wden(:,i));
-%     pre4_3T(i,:)=observe4_3Wden(:,i)';
-%     observe4_3std(1,i)=std(pre4_3T(i,:),1);
-
+	if flag == 1
+		figure(1);
+		%title('直方图');
+		flag = flag+1;
+	end
+	subplot(4,4,i);
+	hist(observe4_3Wden(:,i),100);
+	title(['第',num2str(i),'测点压力差直方图'])；
+	
+	if flag ==2
+		figure(2);
+		flag = flag+1;
+	end
+	subplot(4,4,i);
+	normplot(temp1);
+	title(['第',num2str(i),'测点压力差累计概率'])；
 end
-temp1=observe4_3Wden(:,1);
-alpha = 0.05;
-[mu, sigma] = normfit(temp1);
-p1 = normcdf(temp1, mu, sigma);
+
+
+%% 校验离散数据是否符合正态分布（针对4月2日压力差的校验）
+%方法一：
+%temp1=observe4_3Wden(:,1);
+%alpha = 0.05;
+%[mu, sigma] = normfit(temp1);
+% p1 = normcdf(temp1, mu, sigma);
 % [H1,s1] = kstest(temp1, [temp1, p1], alpha);
 % if H1 == 0
 %     disp('该数据源服从正态分布。')
 % else
 %     disp('该数据源不服从正态分布。')
 % end
-[H,P,LSTAT,CV] = lillietest(temp1,alpha);
-figure(1);
-title('直方图')
-hist(temp1,100);   %作频数直方图 
-figure(2);
-title('分布的正态性检验 ')
-normplot(temp1);  %分布的正态性检验
+
+%方法二，校验结果参考http://10kn.com/matlab-normality-test/
+%[H,P,LSTAT,CV] = lillietest(temp1,alpha);
+
+%% 直方图和正态累计概率
+%figure(1);
+%title('直方图')
+%hist(temp1,100);   %作频数直方图 
+%figure(2);
+%title('分布的正态性检验 ')
+%normplot(temp1);  %分布的正态性检验
 
 
+
+%% 杂项
 % hist(observe4_3Wden(:,1),100); hold on;
 % [b, x]=hist(observe4_3Wden(:,1),100);
 % num=numel(observe4_3Wden(:,1));
@@ -95,6 +100,24 @@ normplot(temp1);  %分布的正态性检验
 % title('4月3日第一个测点压力变化正态分布的累积分布')
 
 
+%% 正态分布累计概率
+% clear
+% x=-4:0.01:4;
+% miu=0;sigma=1;
+% y1=normpdf(x,miu,sigma);
+% y2=normcdf(x,miu,sigma);
+% figure(1) 
+% subplot 221
+% plot(x,y1)
+% title('正态分布的概率密度')
+% subplot 222
+% plot(x,y2)
+% title('正态分布的累积分布')
+
+ %% point1 数据曲线
+% load('observe4_3.mat');
+% t=1:1:1440;
+% plot(t,observe4_3(:,1));
 
 
 
